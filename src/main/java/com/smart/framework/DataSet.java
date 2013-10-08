d28408424b9d20c3905073a2291c86f76bf74a33
黄勇 <huangyong.java@gmail.com>
2013-10-08 20:21:15 +0800

    【I】代码优化

--- src/main/java/com/smart/framework/DataSet.java
+++ src/main/java/com/smart/framework/DataSet.java
@@ -10,44 +10,52 @@ import java.util.Map;
 
 public class DataSet {
 
+    // 查询单条数据，并转为对象
     public static <T> T select(Class<T> cls, String condition, Object... params) {
         String sql = SQLHelper.generateSelectSQL(cls, condition, params);
         return DBHelper.queryBean(cls, sql);
     }
 
+    // 查询多条数据，并转为列表
     public static <T> List<T> selectList(Class<T> cls, String condition, String order, Object... params) {
         String sql = SQLHelper.generateSelectSQL(cls, condition, order, params);
         return DBHelper.queryBeanList(cls, sql);
     }
 
+    // 插入一条数据
     public static boolean insert(Class<?> cls, Map<String, Object> fieldMap) {
         String sql = SQLHelper.generateInsertSQL(cls, fieldMap);
         int rows = DBHelper.update(sql);
         return rows > 0;
     }
 
+    // 更新相关数据
     public static boolean update(Class<?> cls, Map<String, Object> fieldMap, String condition, Object... params) {
         String sql = SQLHelper.generateUpdateSQL(cls, fieldMap, condition, params);
         int rows = DBHelper.update(sql);
         return rows > 0;
     }
 
+    // 删除相关数据
     public static boolean delete(Class<?> cls, String condition, Object... params) {
         String sql = SQLHelper.generateDeleteSQL(cls, condition, params);
         int rows = DBHelper.update(sql);
         return rows > 0;
     }
 
+    // 查询数据条数
     public static int selectCount(Class<?> cls, String condition, Object... params) {
         String sql = SQLHelper.generateSelectSQLForCount(cls, condition, params);
         return DBHelper.queryCount(cls, sql);
     }
 
+    // 查询多条数据，并转为列表（分页方式）
     public static <T> List<T> selectListForPager(int pageNumber, int pageSize, Class<T> cls, String condition, String sort, Object... params) {
         String sql = SQLHelper.generateSelectSQLForPager(pageNumber, pageSize, cls, condition, sort, params);
         return DBHelper.queryBeanList(cls, sql);
     }
 
+    // 查询多条数据，并转为映射
     public static <T> Map<Long, T> selectMap(Class<T> cls, String condition, Object... params) {
         Map<Long, T> map = new HashMap<Long, T>();
         List<T> list = selectList(cls, condition, "", params);
--- src/main/java/com/smart/framework/bean/Result.java
+++ src/main/java/com/smart/framework/bean/Result.java
@@ -4,44 +4,53 @@ import com.smart.framework.base.BaseBean;
 
 public class Result extends BaseBean {
 
-    private boolean success = true;
-    private int error = 0;
-    private Object data = null;
+    private boolean success = true; // 成功标志
+    private int error = 0;          // 错误代码
+    private Object data = null;     // 相关数据
 
+    // 构造结果对象（设置成功标志）
     public Result(boolean success) {
         this.success = success;
     }
 
+    // 构造结果对象（添加相关数据）
     public Result data(Object data) {
         this.data = data;
         return this;
     }
 
+    // 构造结果对象（添加错误代码）
     public Result error(int error) {
         this.error = error;
         return this;
     }
 
+    // 判断是否成功
     public boolean isSuccess() {
         return success;
     }
 
+    // 设置成功标志
     public void setSuccess(boolean success) {
         this.success = success;
     }
 
+    // 获取错误代码
     public int getError() {
         return error;
     }
 
+    // 设置错误代码
     public void setError(int error) {
         this.error = error;
     }
 
+    // 获取相关数据
     public Object getData() {
         return data;
     }
 
+    // 设置相关数据
     public void setData(Object data) {
         this.data = data;
     }
