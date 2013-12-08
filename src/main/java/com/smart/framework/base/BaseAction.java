e93639a70dbcb79a392af52c5465b6240b27ebf2
黄勇 <huangyong.java@gmail.com>
2013-12-08 11:30:09 +0800

    【I】代码重构

--- src/main/java/com/smart/framework/base/BaseAction.java
+++ src/main/java/com/smart/framework/base/BaseAction.java
@@ -4,8 +4,4 @@ public abstract class BaseAction {
 
     protected static final String PAGE_NUMBER = "pageNumber";
     protected static final String PAGE_SIZE = "pageSize";
-    protected static final String QUERY_STRING = "queryString";
-
-    protected static final int ERROR_PARAM = 10; // 参数有误
-    protected static final int ERROR_DATA = 20;  // 数据有误
 }
