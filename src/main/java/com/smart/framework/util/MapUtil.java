06e25a738bb6e4bc69a0f2e5237f85054ad43b8e
黄勇 <huangyong.java@gmail.com>
2013-12-24 11:24:48 +0800

    【F】提供 Map 的 invert 方法

--- src/main/java/com/smart/framework/util/MapUtil.java
+++ src/main/java/com/smart/framework/util/MapUtil.java
@@ -1,5 +1,6 @@
 package com.smart.framework.util;
 
+import java.util.LinkedHashMap;
 import java.util.Map;
 import org.apache.commons.collections.MapUtils;
 
@@ -16,8 +17,11 @@ public class MapUtil {
     }
 
     // 转置 Map
-    @SuppressWarnings("unchecked")
-    public static <K, V> Map<V, K> inverse(Map<K, V> map) {
-        return MapUtils.invertMap(map);
+    public static <K, V> Map<V, K> invert(Map<K, V> source) {
+        Map<V, K> target = new LinkedHashMap<V, K>(source.size());
+        for (Map.Entry<K, V> entry : source.entrySet()) {
+            target.put(entry.getValue(), entry.getKey());
+        }
+        return target;
     }
 }
