e615a3e1b328af61b07b112a404213a728a2be1c
黄勇 <huangyong.java@gmail.com>
2014-04-01 16:19:50 +0800

    【F】在 DatabaseHelper 中添加了几个新的工具方法

--- smart-framework/src/main/java/com/smart/framework/helper/DatabaseHelper.java
+++ smart-framework/src/main/java/com/smart/framework/helper/DatabaseHelper.java
@@ -8,16 +8,23 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import javax.sql.DataSource;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.apache.commons.dbutils.BasicRowProcessor;
 import org.apache.commons.dbutils.BeanProcessor;
 import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.ArrayHandler;
+import org.apache.commons.dbutils.handlers.ArrayListHandler;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.BeanMapHandler;
 import org.apache.commons.dbutils.handlers.ColumnListHandler;
+import org.apache.commons.dbutils.handlers.KeyedHandler;
+import org.apache.commons.dbutils.handlers.MapHandler;
 import org.apache.commons.dbutils.handlers.MapListHandler;
 import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.slf4j.Logger;
@@ -42,12 +49,10 @@ public class DatabaseHelper {
         }
     }
 
-    // 获取数据库类型
     public static String getDatabaseType() {
         return databaseType;
     }
 
-    // 获取数据源
     public static DataSource getDataSource() {
         // 从配置文件中读取 JDBC 配置项
         String driver = ConfigHelper.getConfigString("jdbc.driver");
@@ -93,7 +98,6 @@ public class DatabaseHelper {
         return conn;
     }
 
-    // 开启事务
     public static void beginTransaction() {
         Connection conn = getConnection();
         if (conn != null) {
@@ -108,7 +112,6 @@ public class DatabaseHelper {
         }
     }
 
-    // 提交事务
     public static void commitTransaction() {
         Connection conn = getConnection();
         if (conn != null) {
@@ -124,7 +127,6 @@ public class DatabaseHelper {
         }
     }
 
-    // 回滚事务
     public static void rollbackTransaction() {
         Connection conn = getConnection();
         if (conn != null) {
@@ -140,15 +142,14 @@ public class DatabaseHelper {
         }
     }
 
-    // 执行查询（返回一个对象）
-    public static <T> T queryBean(Class<T> cls, String sql, Object... params) {
+    public static <T> T queryEntity(Class<T> entityClass, String sql, Object... params) {
         T result;
         try {
-            Map<String, String> fieldMap = EntityHelper.getEntityMap().get(cls);
+            Map<String, String> fieldMap = EntityHelper.getEntityMap().get(entityClass);
             if (MapUtil.isNotEmpty(fieldMap)) {
-                result = queryRunner.query(sql, new BeanHandler<T>(cls, new BasicRowProcessor(new BeanProcessor(fieldMap))), params);
+                result = queryRunner.query(sql, new BeanHandler<T>(entityClass, new BasicRowProcessor(new BeanProcessor(fieldMap))), params);
             } else {
-                result = queryRunner.query(sql, new BeanHandler<T>(cls), params);
+                result = queryRunner.query(sql, new BeanHandler<T>(entityClass), params);
             }
         } catch (SQLException e) {
             logger.error("查询出错！", e);
@@ -158,15 +159,14 @@ public class DatabaseHelper {
         return result;
     }
 
-    // 执行查询（返回多个对象）
-    public static <T> List<T> queryBeanList(Class<T> cls, String sql, Object... params) {
+    public static <T> List<T> queryEntityList(Class<T> entityClass, String sql, Object... params) {
         List<T> result;
         try {
-            Map<String, String> fieldMap = EntityHelper.getEntityMap().get(cls);
+            Map<String, String> fieldMap = EntityHelper.getEntityMap().get(entityClass);
             if (MapUtil.isNotEmpty(fieldMap)) {
-                result = queryRunner.query(sql, new BeanListHandler<T>(cls, new BasicRowProcessor(new BeanProcessor(fieldMap))), params);
+                result = queryRunner.query(sql, new BeanListHandler<T>(entityClass, new BasicRowProcessor(new BeanProcessor(fieldMap))), params);
             } else {
-                result = queryRunner.query(sql, new BeanListHandler<T>(cls), params);
+                result = queryRunner.query(sql, new BeanListHandler<T>(entityClass), params);
             }
         } catch (SQLException e) {
             logger.error("查询出错！", e);
@@ -176,7 +176,119 @@ public class DatabaseHelper {
         return result;
     }
 
-    // 执行更新（包括 UPDATE、INSERT、DELETE）
+    public static <K, V> Map<K, V> queryEntityMap(Class<V> entityClass, String sql, Object... params) {
+        Map<K, V> entityMap;
+        try {
+            entityMap = queryRunner.query(sql, new BeanMapHandler<K, V>(entityClass), params);
+        } catch (SQLException e) {
+            logger.error("查询出错！", e);
+            throw new RuntimeException(e);
+        }
+        printSQL(sql);
+        return entityMap;
+    }
+
+    public static Object[] queryArray(String sql, Object... params) {
+        Object[] array;
+        try {
+            array = queryRunner.query(sql, new ArrayHandler(), params);
+        } catch (SQLException e) {
+            logger.error("查询出错！", e);
+            throw new RuntimeException(e);
+        }
+        printSQL(sql);
+        return array;
+    }
+
+    public static List<Object[]> queryArrayList(String sql, Object... params) {
+        List<Object[]> arrayList;
+        try {
+            arrayList = queryRunner.query(sql, new ArrayListHandler(), params);
+        } catch (SQLException e) {
+            logger.error("查询出错！", e);
+            throw new RuntimeException(e);
+        }
+        printSQL(sql);
+        return arrayList;
+    }
+
+    public static Map<String, Object> queryMap(String sql, Object... params) {
+        Map<String, Object> map;
+        try {
+            map = queryRunner.query(sql, new MapHandler(), params);
+        } catch (SQLException e) {
+            logger.error("查询出错！", e);
+            throw new RuntimeException(e);
+        }
+        printSQL(sql);
+        return map;
+    }
+
+    public static List<Map<String, Object>> queryMapList(String sql, Object... params) {
+        List<Map<String, Object>> fieldMapList;
+        try {
+            fieldMapList = queryRunner.query(sql, new MapListHandler(), params);
+        } catch (SQLException e) {
+            logger.error("查询出错！", e);
+            throw new RuntimeException(e);
+        }
+        printSQL(sql);
+        return fieldMapList;
+    }
+
+    public static <T> T queryColumn(String sql, Object... params) {
+        T entity;
+        try {
+            entity = queryRunner.query(sql, new ScalarHandler<T>(), params);
+        } catch (SQLException e) {
+            logger.error("查询出错！", e);
+            throw new RuntimeException(e);
+        }
+        printSQL(sql);
+        return entity;
+    }
+
+    public static <T> List<T> queryColumnList(String sql, Object... params) {
+        List<T> list;
+        try {
+            list = queryRunner.query(sql, new ColumnListHandler<T>(), params);
+        } catch (SQLException e) {
+            logger.error("查询出错！", e);
+            throw new RuntimeException(e);
+        }
+        printSQL(sql);
+        return list;
+    }
+
+    public static <T> Set<T> queryColumnSet(String sql, Object... params) {
+        List<T> list = queryColumnList(sql, params);
+        return new LinkedHashSet<T>(list);
+    }
+
+    public static <T> Map<T, Map<String, Object>> queryColumnMap(String column, String sql, Object... params) {
+        Map<T, Map<String, Object>> map;
+        try {
+            map = queryRunner.query(sql, new KeyedHandler<T>(column), params);
+        } catch (SQLException e) {
+            logger.error("查询出错！", e);
+            throw new RuntimeException(e);
+        }
+        printSQL(sql);
+        return map;
+    }
+
+    public static long queryCount(String sql, Object... params) {
+        long result;
+        try {
+            result = queryRunner.query(sql, new ScalarHandler<Long>("count(*)"), params);
+        } catch (SQLException e) {
+            logger.error("查询出错！", e);
+            throw new RuntimeException(e);
+        }
+        printSQL(sql);
+        return result;
+    }
+
     public static int update(String sql, Object... params) {
         int result;
         try {
@@ -190,59 +302,6 @@ public class DatabaseHelper {
         return result;
     }
 
-    // 执行查询（返回 count 结果）
-    public static long queryCount(String sql, Object... params) {
-        long result;
-        try {
-            result = queryRunner.query(sql, new ScalarHandler<Long>("count(*)"), params);
-        } catch (SQLException e) {
-            logger.error("查询出错！", e);
-            throw new RuntimeException(e);
-        }
-        printSQL(sql);
-        return result;
-    }
-
-    // 查询映射列表
-    public static List<Map<String, Object>> queryMapList(String sql, Object... params) {
-        List<Map<String, Object>> result;
-        try {
-            result = queryRunner.query(sql, new MapListHandler(), params);
-        } catch (SQLException e) {
-            logger.error("查询出错！", e);
-            throw new RuntimeException(e);
-        }
-        printSQL(sql);
-        return result;
-    }
-
-    // 查询单列数据（返回一个对象）
-    public static <T> T queryColumn(String column, String sql, Object... params) {
-        T result;
-        try {
-            result = queryRunner.query(sql, new ScalarHandler<T>(column), params);
-        } catch (SQLException e) {
-            logger.error("查询出错！", e);
-            throw new RuntimeException(e);
-        }
-        printSQL(sql);
-        return result;
-    }
-
-    // 查询单列数据（返回多个对象）
-    public static <T> List<T> queryColumnList(String column, String sql, Object... params) {
-        List<T> result;
-        try {
-            result = queryRunner.query(sql, new ColumnListHandler<T>(column), params);
-        } catch (SQLException e) {
-            logger.error("查询出错！", e);
-            throw new RuntimeException(e);
-        }
-        printSQL(sql);
-        return result;
-    }
-
-    // 插入（返回自动生成的主键）
     public static Serializable insertReturnPK(String sql, Object... params) {
         Serializable key = null;
         Connection conn = getConnection();
