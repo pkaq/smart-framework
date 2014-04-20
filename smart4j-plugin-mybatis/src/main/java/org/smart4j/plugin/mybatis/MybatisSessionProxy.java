61b0c811d0e9f1c5b93d14ef42697e0624158f65
abel533 <abel533@gmail.com>
2014-04-20 14:37:19 +0800

    【I】mybatis插件增加事务传播

--- smart4j-plugin-mybatis/src/main/java/org/smart4j/plugin/mybatis/MybatisSessionProxy.java
+++ smart4j-plugin-mybatis/src/main/java/org/smart4j/plugin/mybatis/MybatisSessionProxy.java
@@ -15,6 +15,24 @@ import org.smart4j.framework.tx.annotation.Service;
 @Aspect(annotation = Service.class)
 public class MybatisSessionProxy extends AspectProxy {
     private static final Logger logger = LoggerFactory.getLogger(MybatisSessionProxy.class);
+
+    // 定义一个线程局部变量，用于保存当前线程中事务处理的阶段，初始为0
+    private static final ThreadLocal<Short> flagContainer = new ThreadLocal<Short>() {
+        protected Short initialValue() {
+            return 0;
+        }
+    };
+
+    private static void incrementFlag() {
+        short flag = flagContainer.get();
+        flagContainer.set(++flag);
+    }
+
+    private static void decrementFlag() {
+        short flag = flagContainer.get();
+        flagContainer.set(--flag);
+    }
+
     /**包扫描路径,逗号隔开*/
     public static final String MYBATIS_SESSION = "mybatis.session.auto";
 
@@ -36,20 +54,44 @@ public class MybatisSessionProxy extends AspectProxy {
 
     @Override
     public void before(Class<?> cls, Method method, Object[] params) throws Throwable {
-        //开启Session
-        if(auto || method.isAnnotationPresent(MybatisSession.class)){
-            MybatisHelper.getSqlSession();
+        //判断是否需要Session
+        if (auto || method.isAnnotationPresent(MybatisSession.class)) {
+            //初始进入方法 - 开启Session
+            if (flagContainer.get() == 0) {
+                MybatisHelper.getSqlSession();
+            }
+            //+1
+            incrementFlag();
+        }
+    }
+
+    @Override
+    public void after(Class<?> cls, Method method, Object[] params, Object result) throws Throwable {
+        //-1
+        if (auto || method.isAnnotationPresent(MybatisSession.class)) {
+            decrementFlag();
         }
     }
 
     @Override
     public void error(Class<?> cls, Method method, Object[] params, Throwable e) {
-        MybatisHelper.rollback();
-        logger.error("操作异常,Mybatis自动回滚,错误信息:"+e.getMessage());
+        //出现异常的时候，进不去after，需要在这里-1
+        if (auto || method.isAnnotationPresent(MybatisSession.class)) {
+            decrementFlag();
+        }
+        //只有返回到最后一层，才会回滚，如果中间catch了异常，这里不会自动回滚
+        if (flagContainer.get() == 0) {
+            MybatisHelper.rollback();
+            logger.error("操作异常,Mybatis自动回滚,错误信息:"+e.getMessage());
+        }
     }
 
     @Override
     public void end() {
-        MybatisHelper.closeSession();
+        if (flagContainer.get() == 0) {
+            //移除
+            flagContainer.remove();
+            MybatisHelper.closeSession();
+        }
     }
 }
