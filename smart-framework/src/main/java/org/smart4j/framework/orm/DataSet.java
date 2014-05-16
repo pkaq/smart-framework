d467922181f9217a0ee746da4ad390af606bcfc8
黄勇 <huangyong.java@gmail.com>
2014-05-17 00:47:26 +0800

    【I】代码优化

--- smart-framework/src/main/java/org/smart4j/framework/orm/DataSet.java
+++ smart-framework/src/main/java/org/smart4j/framework/orm/DataSet.java
@@ -20,7 +20,7 @@ import org.smart4j.framework.util.StringUtil;
 public class DataSet {
 
     /**
-     * 查询单条数据，并转为相应类型的对象
+     * 查询单条数据，并转为相应类型的实体
      */
     public static <T> T select(Class<T> entityClass, String condition, Object... params) {
         condition = transfer(condition);
@@ -29,102 +29,34 @@ public class DataSet {
     }
 
     /**
-     * 查询多条数据，并转为相应类型的列表
+     * 查询多条数据，并转为相应类型的实体列表
      */
     public static <T> List<T> selectList(Class<T> entityClass) {
-        return selectList(entityClass, "", "");
+        return selectListWithConditionAndSort(entityClass, "", "");
     }
 
     /**
-     * 查询多条数据，并转为相应类型的列表（带有条件、排序、参数）
+     * 查询多条数据，并转为相应类型的实体列表（带有查询条件与查询参数）
      */
-    public static <T> List<T> selectList(Class<T> entityClass, String condition, String sort, Object... params) {
+    public static <T> List<T> selectListWithCondition(Class<T> entityClass, String condition, Object... params) {
+        return selectListWithConditionAndSort(entityClass, condition, "", params);
+    }
+
+    /**
+     * 查询多条数据，并转为相应类型的实体列表（带有排序方式）
+     */
+    public static <T> List<T> selectListWithSort(Class<T> entityClass, String sort) {
+        return selectListWithConditionAndSort(entityClass, "", sort);
+    }
+
+    /**
+     * 查询多条数据，并转为相应类型的实体列表（带有查询条件、排序方式与查询参数）
+     */
+    public static <T> List<T> selectListWithConditionAndSort(Class<T> entityClass, String condition, String sort, Object... params) {
         condition = transfer(condition);
         sort = transfer(sort);
         String sql = SqlHelper.generateSelectSql(entityClass, condition, sort);
         return DatabaseHelper.queryEntityList(entityClass, sql, params);
-    }
-
-    /**
-     * 插入一条数据
-     */
-    public static boolean insert(Class<?> entityClass, Map<String, Object> fieldMap) {
-        String sql = SqlHelper.generateInsertSql(entityClass, fieldMap.keySet());
-        int rows = DatabaseHelper.update(sql, fieldMap.values().toArray());
-        return rows > 0;
-    }
-
-    /**
-     * 插入一个实体对象
-     */
-    public static boolean insert(Object entity) {
-        if (entity == null) {
-            throw new IllegalArgumentException();
-        }
-        Class<?> entityClass = entity.getClass();
-        Map<String, Object> fieldMap = ObjectUtil.getFieldMap(entity);
-        return insert(entityClass, fieldMap);
-    }
-
-    /**
-     * 更新相关数据
-     */
-    public static boolean update(Class<?> entityClass, Map<String, Object> fieldMap, String condition, Object... params) {
-        condition = transfer(condition);
-        String sql = SqlHelper.generateUpdateSql(entityClass, fieldMap, condition);
-        int rows = DatabaseHelper.update(sql, ArrayUtil.concat(fieldMap.values().toArray(), params));
-        return rows > 0;
-    }
-
-    /**
-     * 更新一个实体对象
-     */
-    public static boolean update(Object entity) {
-        return update(entity, "id");
-    }
-
-    /**
-     * 更新一个实体对象（可指定主键字段名）
-     */
-    public static boolean update(Object entityObject, String pkName) {
-        if (entityObject == null) {
-            throw new IllegalArgumentException();
-        }
-        Class<?> entityClass = entityObject.getClass();
-        Map<String, Object> fieldMap = ObjectUtil.getFieldMap(entityObject);
-        String condition = pkName + " = ?";
-        Object[] params = {ObjectUtil.getFieldValue(entityObject, pkName)};
-        return update(entityClass, fieldMap, condition, params);
-    }
-
-    /**
-     * 删除相关数据
-     */
-    public static boolean delete(Class<?> entityClass, String condition, Object... params) {
-        condition = transfer(condition);
-        String sql = SqlHelper.generateDeleteSql(entityClass, condition);
-        int rows = DatabaseHelper.update(sql, params);
-        return rows > 0;
-    }
-
-    /**
-     * 删除一个实体对象
-     */
-    public static boolean delete(Object entityObject) {
-        return delete(entityObject, "id");
-    }
-
-    /**
-     * 删除一个实体对象（可指定主键字段名）
-     */
-    public static boolean delete(Object entityObject, String pkName) {
-        if (entityObject == null) {
-            throw new IllegalArgumentException();
-        }
-        Class<?> entityClass = entityObject.getClass();
-        String condition = pkName + " = ?";
-        Object[] params = {ObjectUtil.getFieldValue(entityObject, pkName)};
-        return delete(entityClass, condition, params);
     }
 
     /**
@@ -150,23 +82,30 @@ public class DataSet {
      * 查询多条数据，并转为映射
      */
     public static <T> Map<Long, T> selectMap(Class<T> entityClass) {
-        return selectMap(entityClass, "id", "");
+        return selectMapWithPK(entityClass, "id", "", "");
     }
 
     /**
-     * 查询多条数据，并转为映射（带有条件、参数）
+     * 查询多条数据，并转为映射（带有查询条件与查询参数）
      */
-    public static <T> Map<Long, T> selectMap(Class<T> entityClass, String condition, Object... params) {
-        return selectMap(entityClass, "id", condition, params);
+    public static <T> Map<Long, T> selectMapWithCondition(Class<T> entityClass, String condition, Object... params) {
+        return selectMapWithPK(entityClass, "id", condition, "", params);
     }
 
     /**
-     * 查询多条数据，并转为映射（可指定主键字段名）
+     * 查询多条数据，并转为映射（带有查询条件、排序方式与查询参数）
+     */
+    public static <T> Map<Long, T> selectMapWithConditionAndSort(Class<T> entityClass, String condition, String sort, Object... params) {
+        return selectMapWithPK(entityClass, "id", condition, sort, params);
+    }
+
+    /**
+     * 查询多条数据，并转为映射（带有主键名）
      */
     @SuppressWarnings("unchecked")
-    public static <PK, T> Map<PK, T> selectMap(Class<T> entityClass, String pkName, String condition, Object... params) {
+    public static <PK, T> Map<PK, T> selectMapWithPK(Class<T> entityClass, String pkName, String condition, String sort, Object... params) {
         Map<PK, T> map = new HashMap<PK, T>();
-        List<T> list = selectList(entityClass, condition, "", params);
+        List<T> list = selectListWithConditionAndSort(entityClass, condition, sort, params);
         for (T obj : list) {
             PK pk = (PK) ObjectUtil.getFieldValue(obj, pkName);
             map.put(pk, obj);
@@ -175,7 +114,7 @@ public class DataSet {
     }
 
     /**
-     * 根据列名查询单条数据，并转为相应类型的对象
+     * 根据列名查询单条数据，并转为相应类型的实体
      */
     public static <T> T selectColumn(Class<T> entityClass, String columnName, String condition, Object... params) {
         condition = transfer(condition);
@@ -184,13 +123,95 @@ public class DataSet {
     }
 
     /**
-     * 根据列名查询多条数据，并转为相应类型的列表
+     * 根据列名查询多条数据，并转为相应类型的实体列表
      */
     public static <T> List<T> selectColumnList(Class<?> entityClass, String columnName, String condition, String sort, Object... params) {
         condition = transfer(condition);
         sort = transfer(sort);
         String sql = SqlHelper.generateSelectSql(entityClass, condition, sort);
         return DatabaseHelper.queryColumnList(columnName, sql, params);
+    }
+
+    /**
+     * 插入一条数据
+     */
+    public static boolean insert(Class<?> entityClass, Map<String, Object> fieldMap) {
+        String sql = SqlHelper.generateInsertSql(entityClass, fieldMap.keySet());
+        int rows = DatabaseHelper.update(sql, fieldMap.values().toArray());
+        return rows > 0;
+    }
+
+    /**
+     * 插入一个实体
+     */
+    public static boolean insert(Object entity) {
+        if (entity == null) {
+            throw new IllegalArgumentException();
+        }
+        Class<?> entityClass = entity.getClass();
+        Map<String, Object> fieldMap = ObjectUtil.getFieldMap(entity);
+        return insert(entityClass, fieldMap);
+    }
+
+    /**
+     * 更新相关数据
+     */
+    public static boolean update(Class<?> entityClass, Map<String, Object> fieldMap, String condition, Object... params) {
+        condition = transfer(condition);
+        String sql = SqlHelper.generateUpdateSql(entityClass, fieldMap, condition);
+        int rows = DatabaseHelper.update(sql, ArrayUtil.concat(fieldMap.values().toArray(), params));
+        return rows > 0;
+    }
+
+    /**
+     * 更新一个实体
+     */
+    public static boolean update(Object entity) {
+        return update(entity, "id");
+    }
+
+    /**
+     * 更新一个实体（带有主键名）
+     */
+    public static boolean update(Object entityObject, String pkName) {
+        if (entityObject == null) {
+            throw new IllegalArgumentException();
+        }
+        Class<?> entityClass = entityObject.getClass();
+        Map<String, Object> fieldMap = ObjectUtil.getFieldMap(entityObject);
+        String condition = pkName + " = ?";
+        Object[] params = {ObjectUtil.getFieldValue(entityObject, pkName)};
+        return update(entityClass, fieldMap, condition, params);
+    }
+
+    /**
+     * 删除相关数据
+     */
+    public static boolean delete(Class<?> entityClass, String condition, Object... params) {
+        condition = transfer(condition);
+        String sql = SqlHelper.generateDeleteSql(entityClass, condition);
+        int rows = DatabaseHelper.update(sql, params);
+        return rows > 0;
+    }
+
+    /**
+     * 删除一个实体
+     */
+    public static boolean delete(Object entityObject) {
+        return delete(entityObject, "id");
+    }
+
+    /**
+     * 删除一个实体（可指定主键名）
+     */
+    public static boolean delete(Object entityObject, String pkName) {
+        if (entityObject == null) {
+            throw new IllegalArgumentException();
+        }
+        Class<?> entityClass = entityObject.getClass();
+        String condition = pkName + " = ?";
+        Object[] params = {ObjectUtil.getFieldValue(entityObject, pkName)};
+        return delete(entityClass, condition, params);
     }
 
     private static String transfer(String expression) {
