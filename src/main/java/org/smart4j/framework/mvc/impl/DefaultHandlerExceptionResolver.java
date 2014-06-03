6c66ed48ddb0915b9bde3e7c6c6b42a2dab1fc40
黄勇 <huangyong.java@gmail.com>
2014-06-04 01:44:11 +0800

    【B】修复 e.getCause() 为 null 的 Bug

--- src/main/java/org/smart4j/framework/mvc/impl/DefaultHandlerExceptionResolver.java
+++ src/main/java/org/smart4j/framework/mvc/impl/DefaultHandlerExceptionResolver.java
@@ -2,6 +2,8 @@ package org.smart4j.framework.mvc.impl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.smart4j.framework.FrameworkConstant;
 import org.smart4j.framework.mvc.HandlerExceptionResolver;
 import org.smart4j.framework.mvc.fault.AuthcException;
@@ -16,10 +18,16 @@ import org.smart4j.framework.util.WebUtil;
  */
 public class DefaultHandlerExceptionResolver implements HandlerExceptionResolver {
 
+    private static final Logger logger = LoggerFactory.getLogger(DefaultHandlerExceptionResolver.class);
+
     @Override
     public void resolveHandlerException(HttpServletRequest request, HttpServletResponse response, Exception e) {
         // 判断异常原因
         Throwable cause = e.getCause();
+        if (cause == null) {
+            logger.error(e.getMessage(), e);
+            return;
+        }
         if (cause instanceof AuthcException) {
             // 分两种情况进行处理
             if (WebUtil.isAJAX(request)) {
