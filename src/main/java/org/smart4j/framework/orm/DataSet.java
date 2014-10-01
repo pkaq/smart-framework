72b34436fa4bced2999316db0957d6de15494999
黄勇 <yong.huang@adchina.com>
2014-10-01 08:30:38 +0800

    v2.3.3-SNAPSHOT - 改进：去掉 transferCondition 与 transferSort 功能

--- src/main/java/org/smart4j/framework/orm/DataSet.java
+++ src/main/java/org/smart4j/framework/orm/DataSet.java
@@ -3,15 +3,12 @@ package org.smart4j.framework.orm;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import org.smart4j.framework.FrameworkConstant;
 import org.smart4j.framework.dao.DatabaseHelper;
 import org.smart4j.framework.dao.SqlHelper;
 import org.smart4j.framework.util.ArrayUtil;
 import org.smart4j.framework.util.MapUtil;
 import org.smart4j.framework.util.ObjectUtil;
-import org.smart4j.framework.util.StringUtil;
 
 /**
  * 提供与实体相关的数据库操作
@@ -25,7 +22,6 @@ public class DataSet {
      * 查询单条数据，并转为相应类型的实体
      */
     public static <T> T select(Class<T> entityClass, String condition, Object... params) {
-        condition = transferCondition(entityClass, condition);
         String sql = SqlHelper.generateSelectSql(entityClass, condition, "");
         return DatabaseHelper.queryEntity(entityClass, sql, params);
     }
@@ -55,8 +51,6 @@ public class DataSet {
      * 查询多条数据，并转为相应类型的实体列表（带有查询条件、排序方式与查询参数）
      */
     public static <T> List<T> selectListWithConditionAndSort(Class<T> entityClass, String condition, String sort, Object... params) {
-        condition = transferCondition(entityClass, condition);
-        sort = transferSort(entityClass, sort);
         String sql = SqlHelper.generateSelectSql(entityClass, condition, sort);
         return DatabaseHelper.queryEntityList(entityClass, sql, params);
     }
@@ -65,7 +59,6 @@ public class DataSet {
      * 查询数据条数
      */
     public static long selectCount(Class<?> entityClass, String condition, Object... params) {
-        condition = transferCondition(entityClass, condition);
         String sql = SqlHelper.generateSelectSqlForCount(entityClass, condition);
         return DatabaseHelper.queryCount(sql, params);
     }
@@ -74,8 +67,6 @@ public class DataSet {
      * 查询多条数据，并转为列表（分页方式）
      */
     public static <T> List<T> selectListForPager(int pageNumber, int pageSize, Class<T> entityClass, String condition, String sort, Object... params) {
-        condition = transferCondition(entityClass, condition);
-        sort = transferSort(entityClass, sort);
         String sql = SqlHelper.generateSelectSqlForPager(pageNumber, pageSize, entityClass, condition, sort);
         return DatabaseHelper.queryEntityList(entityClass, sql, params);
     }
@@ -128,7 +119,6 @@ public class DataSet {
      * 根据列名查询单条数据，并转为相应类型的实体
      */
     public static <T> T selectColumn(Class<?> entityClass, String columnName, String condition, Object... params) {
-        condition = transferCondition(entityClass, condition);
         String sql = SqlHelper.generateSelectSql(entityClass, condition, "");
         sql = sql.replace("*", columnName);
         return DatabaseHelper.queryColumn(sql, params);
@@ -138,8 +128,6 @@ public class DataSet {
      * 根据列名查询多条数据，并转为相应类型的实体列表
      */
     public static <T> List<T> selectColumnList(Class<?> entityClass, String columnName, String condition, String sort, Object... params) {
-        condition = transferCondition(entityClass, condition);
-        sort = transferSort(entityClass, sort);
         String sql = SqlHelper.generateSelectSql(entityClass, condition, sort);
         sql = sql.replace("*", columnName);
         return DatabaseHelper.queryColumnList(sql, params);
@@ -176,7 +164,6 @@ public class DataSet {
         if (MapUtil.isEmpty(fieldMap)) {
             return true;
         }
-        condition = transferCondition(entityClass, condition);
         String sql = SqlHelper.generateUpdateSql(entityClass, fieldMap, condition);
         int rows = DatabaseHelper.update(sql, ArrayUtil.concat(fieldMap.values().toArray(), params));
         return rows > 0;
@@ -207,7 +194,6 @@ public class DataSet {
      * 删除相关数据
      */
     public static boolean delete(Class<?> entityClass, String condition, Object... params) {
-        condition = transferCondition(entityClass, condition);
         String sql = SqlHelper.generateDeleteSql(entityClass, condition);
         int rows = DatabaseHelper.update(sql, params);
         return rows > 0;
@@ -231,39 +217,5 @@ public class DataSet {
         String condition = pkName + " = ?";
         Object[] params = {ObjectUtil.getFieldValue(entityObject, pkName)};
         return delete(entityClass, condition, params);
-    }
-
-    private static String transferCondition(Class<?> entityClass, String condition) {
-        if (StringUtil.isNotEmpty(condition)) {
-            StringBuffer buffer = new StringBuffer();
-            String regex = "([a-z_]+([a-z|A-Z|0-9|_]*)+)\\s*(=|!=|<>|>=|>|<=|<|like)\\s*";
-            Matcher matcher = Pattern.compile(regex).matcher(condition.trim());
-            while (matcher.find()) {
-                String fieldName = matcher.group(1);
-                String columnName = EntityHelper.getColumnName(entityClass, fieldName);
-                String operator = matcher.group(3);
-                matcher.appendReplacement(buffer, columnName);
-                buffer.append(" ").append(operator).append(" ");
-            }
-            matcher.appendTail(buffer);
-            return buffer.toString();
-        }
-        return condition;
-    }
-
-    private static String transferSort(Class<?> entityClass, String sort) {
-        if (StringUtil.isNotEmpty(sort)) {
-            StringBuffer buffer = new StringBuffer();
-            String regex = "([a-z_]+([A-Z]+[a-z|0-9|_]+)+)";
-            Matcher matcher = Pattern.compile(regex).matcher(sort.trim());
-            while (matcher.find()) {
-                String fieldName = matcher.group(1);
-                String columnName = EntityHelper.getColumnName(entityClass, fieldName);
-                matcher.appendReplacement(buffer, columnName);
-            }
-            matcher.appendTail(buffer);
-            return buffer.toString();
-        }
-        return sort;
     }
 }
